44717c9bc41ce9f21d9593862ad247427fb80370
Rain-Gayming <raingayming@gmail.com>
2024-07-29 11:40:48 +0100

    Camera Added

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,20 @@ fn main() {
         //plugins
         .add_plugins(DefaultPlugins)
 
+        //systems
+        .add_systems(Startup, start)
+
         .run();
+}
+
+fn start(
+    mut commands : Commands
+){
+    commands.spawn(
+        Camera3dBundle {
+            transform: Transform::from_xyz(10.0, 12.0, 16.0)
+                .looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        }
+    );
 }
