21e0b42bbab90557a6341fc54655fe8d1fa00377
rain-gayming <raingayming@gmail.com>
2024-09-14 13:34:59 +0100

    i got keyboard inputs working :3

--- src/entities/player.rs
+++ src/entities/player.rs
@@ -1,7 +1,17 @@
-
-use bevy::prelude::*;
+use bevy::{prelude::*};
+use avian2d::{math::*, prelude::*};
 
 use super::entities::*;
+
+
+use crate::plugin;
+use plugin::*;
+
+#[derive(Component, Default)]
+pub struct KeyboardInputs{
+    pub left: bool,
+    pub right: bool,
+}
 
 pub fn setup_player(
     mut commands: Commands, 
@@ -15,22 +25,48 @@ pub fn setup_player(
             ..default()
         },
         KeyboardMovable,
+        KeyboardInputs{
+            ..default()
+        }, 
         EntityValues{
             speed: 15.0,
-        }
+        },
+        CharacterControllerBundle::new(Collider::capsule(12.5, 20.0), Vector::NEG_Y * 1500.0),
     ));
 }
 
 pub fn move_player(
-    mut query: Query<(&mut Transform, &mut EntityValues), With<KeyboardMovable>>,
-    keys: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&mut Transform, &mut EntityValues, &mut KeyboardInputs), With<KeyboardMovable>>,
 ) {
-    for (mut transform, entity_values) in query.iter_mut() {
-        if keys.just_pressed(KeyCode::KeyA) {
+    for (mut transform, entity_values, keyboard_inputs) in query.iter_mut() {
+        if keyboard_inputs.left {
             transform.translation.x -= entity_values.speed;
         }
-        if keys.just_pressed(KeyCode::KeyD) {
+        if keyboard_inputs.right {
             transform.translation.x += entity_values.speed;
         }
     }
 } 
+
+pub fn keyboard_input(
+    mut query: Query<(&mut KeyboardInputs)>,
+    keys: Res<ButtonInput<KeyCode>>,
+){
+
+    for mut keyboard_inputs in query.iter_mut() {
+        
+        if keys.pressed(KeyCode::KeyA) {
+            keyboard_inputs.left = true;
+        }else{
+            keyboard_inputs.left = false;
+        }
+
+
+        if keys.pressed(KeyCode::KeyD) {
+            keyboard_inputs.right = true;
+        }else{
+            keyboard_inputs.right = false;
+        }
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use bevy::{ecs::{query, system::SystemState}, prelude::*, transform};
+use avian2d::{math::*, prelude::*};
+use bevy_asset_loader::prelude::*;
 
 mod entities;
 use entities::player::*;
+
+mod plugin;
+use plugin::*;
 
 
 #[derive(Component)]
@@ -11,13 +16,33 @@ fn main() {
     App::new()
         //plugins
         .add_plugins(DefaultPlugins)
+        .add_plugins(PhysicsPlugins::default())
         
         //startups
         .add_systems(Startup, (setup_camera, setup_player))
 
         //updates
         .add_systems(Update, update)
-        .add_systems(Update, move_player)
+
+        //player
+        .add_systems(Update, 
+    (
+                move_player,
+                keyboard_input,
+            )
+        )
+
+        //resources
+        .insert_resource(Gravity(Vector::NEG_Y * 1000.0))
+
+        //asset loader
+        .init_state::<MyStates>()
+        .add_loading_state(
+            LoadingState::new(MyStates::AssetLoading)
+                .continue_to_state(MyStates::Next)
+                .load_collection::<AudioAssets>(),
+        )
+        .add_systems(OnEnter(MyStates::Next), start_background_audio)
 
         .run();
 }
@@ -42,4 +67,26 @@ fn setup_camera(mut commands: Commands) {
         },
         MyCameraMarker,
     ));
+}
+
+#[derive(AssetCollection, Resource)]
+struct AudioAssets {
+    #[asset(path = "audio/background.ogg")]
+    background: Handle<AudioSource>,
+}
+
+/// This system runs in MyStates::Next. Thus, AudioAssets is available as a resource
+/// and the contained handle is done loading.
+fn start_background_audio(mut commands: Commands, audio_assets: Res<AudioAssets>) {
+    commands.spawn(AudioBundle {
+        source: audio_assets.background.clone(),
+        settings: PlaybackSettings::LOOP,
+    });
+}
+
+#[derive(Clone, Eq, PartialEq, Debug, Hash, Default, States)]
+enum MyStates {
+    #[default]
+    AssetLoading,
+    Next,
 }
